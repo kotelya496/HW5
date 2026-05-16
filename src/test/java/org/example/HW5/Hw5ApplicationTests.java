33683ff289c60074360a9d5c68207315bc496eb5
Ivan Kotelya <kotelya496@gmail.com>
2026-05-16 21:42:12 +0600

    Init project

---
+++ src/test/java/org/example/HW5/Hw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.HW5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
